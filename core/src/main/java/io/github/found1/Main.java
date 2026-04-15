465094257e0f19193ee924714476ba0732844365
stephjac000 <stephjac000@warren.k12.in.us>
2026-04-15 14:00:03 -0400

    initial commit

---
+++ core/src/main/java/io/github/found1/Main.java
@@ -0,0 +1,12 @@
+package io.github.found1;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+
+    @Override
+    public void create() {
+        setScreen(new AnimationDemoScreen());
+    }
+}
